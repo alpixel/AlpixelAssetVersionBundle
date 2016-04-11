9ca5d88dcdbf897d8f4eb51cd4eedc02c7a42e3b
Benjamin HUBERT <benjamin@alpixel.fr>
2016-04-11 19:16:30 +0200

    initial import

---
+++ AlpixelAssetVersionBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alpixel\Bundle\AssetVersionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+/**
+ * @author Benjamin HUBERT <benjamin@alpixel.fr>
+ */
+class AlpixelAssetVersionBundle extends Bundle
+{
+
+}
